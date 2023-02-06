b23442612828d6f51b502c0f1d15391c72ea6f82
Katia <prosto-Katuha@mail.ru>
2023-02-06 16:02:52 +0300

    domaschka

---
+++ ZADACA21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21: Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние между ними в 3D 
+// пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int InputNumber(string str)
+{
+System.Console.WriteLine(str);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = InputNumber("Введите координату x1: ");
+int y1 = InputNumber("Введите координату y1: ");
+int z1 = InputNumber("Введите координату z1: ");
+
+int x2 = InputNumber("Введите координату x2: ");
+int y2 = InputNumber("Введите координату y2: ");
+int z2 = InputNumber("Введите координату z2: ");
+
+System.Console.WriteLine(Math.Round(Math.Pow(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2),0.5),2));
---
+++ zadacha23/Program.cs
@@ -0,0 +1,21 @@
+﻿ // Задача 23: Напишите программу, которая принимает на вход число (N) и
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27 
+// 5 -> 1, 8, 27, 64, 125
+
+System.Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// int count = 1;
+
+// while (count <= N)
+// {
+// System.Console.Write(Math.Pow(count, 3) + ", ");
+// count+++;
+// }
+
+for (int count = 1; count <= N; count++)
+{
+System.Console.Write(Math.Pow(count, 3) + ", ");
+}
